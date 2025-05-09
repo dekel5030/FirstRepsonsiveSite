b97e16077c019f71914e1c30bb66a730daf37c07
Dekel5030 <dekel5030@gmail.com>
2025-05-09 10:32:54 +0300

    fix: correct viewport meta tag to apply mobile styles properly

--- src/index.js
+++ src/index.js
@@ -51,5 +51,3 @@ for (let i = 0; i < 8; i++) {
           <div class="project-description">Short description of the project. Just a couple sentences will do.</div> 
         </article>`;
 }
-
-alert(window.innerWidth);
